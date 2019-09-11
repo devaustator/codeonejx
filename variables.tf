6dbed53e487265d81fd92c8004a4554c761092d1
evenalainen <erno.venalainen@oracle.com>
2019-09-11 13:46:58 +0300

    Uncommented private key variable line

--- variables.tf
+++ variables.tf
@@ -3,5 +3,5 @@ variable "compartment_ocid" {}
 variable "user_ocid" {}
 variable "fingerprint" {}
 variable "private_key_path" {}
-#variable "private_key_password" {}
+variable "private_key_password" {}
 variable "region" {}
